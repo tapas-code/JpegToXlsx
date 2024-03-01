70ee1445497f972be90d1fdd304aa11fe89621b6
Tapas <tapashkumar.tanty@spikewell.com>
2024-03-01 10:51:33 +0530

    Added Toastify Notification

--- client/src/components/FileUploader.jsx
+++ client/src/components/FileUploader.jsx
@@ -1,8 +1,21 @@
 import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const FileUploader = () => {
   const [files, setFiles] = useState([]);
   const [uploadStatus, setUploadStatus] = useState(false);
+  const notify = () =>
+    toast.success("File Uploaded Successfully.", {
+      position: "top-right",
+      autoClose: 1500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: false,
+      progress: undefined,
+      theme: "dark",
+    });
 
   const handleChange = (event) => {
     const uploadedFiles = Array.from(event.target.files);
@@ -52,68 +65,86 @@ const FileUploader = () => {
   };
 
   return (
-    <div className="py-12">
-      <div className="max-w-7xl mx-auto sm-px-6 lg:px-8">
-        <div
-          id="container"
-          className="flex flex-col items-center px-6 py-12 border-4 border-dashed border-gray-600 rounded-xl "
-          onDragOver={handleDragOver}
-          onDragLeave={handleDragLeave}
-          onDrop={handleDrop}
-        >
-          <svg
-            className="w-12 h-12 text-indigo-400 mb-4"
-            aria-hidden="true"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 48 48"
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={1500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable={false}
+        pauseOnHover={false}
+        theme="dark"
+      />
+      <div className="py-12">
+        <div className="max-w-7xl mx-auto sm-px-6 lg:px-8">
+          <div
+            id="container"
+            className="flex flex-col items-center px-6 py-12 border-4 border-dashed border-gray-600 rounded-xl "
+            onDragOver={handleDragOver}
+            onDragLeave={handleDragLeave}
+            onDrop={handleDrop}
           >
-            <path
-              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-            />
-          </svg>
+            <svg
+              className="w-12 h-12 text-indigo-400 mb-4"
+              aria-hidden="true"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 48 48"
+            >
+              <path
+                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+              />
+            </svg>
 
-          <p className="text-xl text-gray-300">Drag and Drop files to upload</p>
-          <p>or</p>
+            <p className="text-xl text-gray-300">
+              Drag and Drop files to upload
+            </p>
+            <p>or</p>
 
-          {uploadStatus ? (
-            <>
-              <button
-                className="mt-2 font-medium shadow-lg hover:shadow-sm hover:shadow-indigo-900"
-                onClick={handleUpload}
-              >
-                Upload
-              </button>
-              <p className="text-xs text-neutral-400 mt-6">
-                Maximum upload file size: 15MB.
-              </p>
-            </>
-          ) : (
-            <>
-              <label className="mt-2 font-medium shadow-lg hover:shadow-sm hover:shadow-indigo-900">
-                Select Files
-                <input
-                  type="file"
-                  name="file"
-                  multiple
-                  className="sr-only"
-                  onChange={handleChange}
-                />
-              </label>
-              <p className="text-xs text-neutral-400 mt-6">
-                Maximum upload file size: 15MB.
-              </p>
-            </>
-          )}
-        </div>
-        <div id="preview" className="grid grid-cols-3 grid-rows-3 mt-8">
-          {/* Image previews will be displayed here */}
+            {uploadStatus ? (
+              <>
+                <button
+                  className="mt-2 font-medium shadow-lg hover:shadow-sm hover:shadow-indigo-900 hover:border-green-500"
+                  onClick={() => {
+                    handleUpload(), notify();
+                  }}
+                >
+                  Upload
+                </button>
+                <p className="text-xs text-neutral-400 mt-6">
+                  Click the button to upload files.
+                </p>
+              </>
+            ) : (
+              <>
+                <label className="mt-2 font-medium shadow-lg hover:shadow-sm hover:shadow-indigo-900">
+                  Select Files
+                  <input
+                    type="file"
+                    name="file"
+                    multiple
+                    className="sr-only"
+                    onChange={handleChange}
+                  />
+                </label>
+                <p className="text-xs text-neutral-400 mt-6">
+                  Maximum upload file size: 15MB.
+                </p>
+              </>
+            )}
+          </div>
+          <div id="preview" className="grid grid-cols-3 grid-rows-3 mt-8">
+            {/* Image previews will be displayed here */}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
